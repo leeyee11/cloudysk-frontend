bee85478a910b4076abfd8863e06b28ed464b801
Lee Yee <leeyee11@qq.com>
2023-02-11 16:44:50 +0800

    chore: remove unused code (#4)

--- src/access.ts
+++ src/access.ts
@@ -1,3 +1,5 @@
+import { API } from './services/typings';
+
 export default (initialState: API.UserInfo) => {
   // 在这里按照初始化数据定义项目中的权限，统一管理
   // 参考文档 https://next.umijs.org/docs/max/access
--- src/services/UserController.ts
+++ src/services/UserController.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+import { API } from './typings';
 import { request } from '@umijs/max';
 
 /** 此处后端没有提供注释 GET /api/v1/queryUserList */
